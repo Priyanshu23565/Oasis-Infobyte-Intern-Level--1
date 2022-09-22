1b51a93356311b589bda561d91680ab2b41d0892
Priyanshu <97750379+Priyanshu23565@users.noreply.github.com>
2022-09-22 18:16:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new typed(".typing", {
+    strings: ["Web Developer", "Designer", "Programmer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Web Developer", "Designer", "Programmer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
